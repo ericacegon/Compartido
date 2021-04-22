0f27d469be08fc838508e0f205feb21f5cea4089
eric.acegon <eric.acegon@educa.jcyl.es>
2021-04-22 18:37:24 +0200

    Introduzco variables con sus getters y setters, e introduzco el método suma.

---
+++ Compartido/src/Principal.java
@@ -0,0 +1,42 @@
+
+public class Principal {
+
+	public double num1;
+	public double num2;
+	public double resultado;
+	
+	public double getNum1() {
+		return num1;
+	}
+
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+
+	public double getNum2() {
+		return num2;
+	}
+
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+
+	public double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+	
+	public double suma(double a, double b) {
+		return a + b;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
