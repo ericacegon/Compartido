be1db9cacce34650d6d0f1d5447e526c7df48583
pablopriant <pablo.priant@educa.jcyl.es>
2021-04-27 18:35:21 +0200

    añado metodo resta

--- Compartido/src/Principal.java
+++ Compartido/src/Principal.java
@@ -29,14 +29,17 @@ public class Principal {
 		this.resultado = resultado;
 	}
 	
-	public double suma(double a, double b) {
+	public static double suma(double a, double b) {
 		return a + b;
 	}
 
+	public static double resta(double a, double b) {
+		return a - b;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
