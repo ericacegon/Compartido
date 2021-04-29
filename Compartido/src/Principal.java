06fec86b688e666686991ac855a7de6b69ba41c0
pablopriant <pablo.priant@educa.jcyl.es>
2021-04-29 17:46:02 +0200

    completo el main

--- Compartido/src/Principal.java
+++ Compartido/src/Principal.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class Principal {
 
 	public double num1;
 	public double num2;
 	public double resultado;
-	
+
 	public double getNum1() {
 		return num1;
 	}
@@ -28,7 +29,7 @@ public class Principal {
 	public void setResultado(double resultado) {
 		this.resultado = resultado;
 	}
-	
+
 	public static double suma(double a, double b) {
 		return a + b;
 	}
@@ -36,18 +37,62 @@ public class Principal {
 	public static double resta(double a, double b) {
 		return a - b;
 	}
-	
+
 	public static double multiplicacion(double a, double b) {
 		return a * b;
 	}
-	
+
 	public static double division(double a, double b) {
 		return a / b;
 	}
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		int op, n1, n2;
+		double resul;
+		String continuar = "";
+
+		do {
+			
+			System.out.println("¿Qué operación quieres realizar?");
+			System.out.println("1. SUMA");
+			System.out.println("2. RESTA");
+			System.out.println("3. MULTIPLICACIÓN");
+			System.out.println("4. DIVISIÓN");
+			op = sc.nextInt();
+
+			System.out.println("Introduce el primer número");
+			n1 = sc.nextInt();
+			System.out.println("Introduce el segundo número");
+			n2 = sc.nextInt();
+
+			if (op == 1) {
+				resul=suma(n1,n2);
+				System.out.println("El resultado de la suma de "+n1+" y "+n2+" es "+resul);
+			}
+			if (op == 2) {
+				resul=resta(n1,n2);
+				System.out.println("El resultado de la resta de "+n1+" y "+n2+" es "+resul);
+			}
+			if (op == 3) {
+				resul=multiplicacion(n1,n2);
+				System.out.println("El resultado de la multiplicación de "+n1+" y "+n2+" es "+resul);
+			}
+			if (op == 4) {
+				resul=division(n1,n2);
+				System.out.println("El resultado de la división de "+n1+" y "+n2+" es "+resul);
+			}
+
+			System.out.println();
+			System.out.println("¿Quieres realizar otra operación?");
+			continuar=sc.next();
+			
+		} while (continuar.equals("si"));
+
+		System.out.println("== FIN ==");
+
 	}
 
 }
